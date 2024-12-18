dd6e56c3cb20be0fe45c7739fb9708fe0e5f49b7
TanishaMehta17 <121849870+TanishaMehta17@users.noreply.github.com>
2024-12-18 19:23:23 +0530

    firstCommit

---
+++ src/test/java/com/example/healthapp/repository/UserRepo.java
@@ -0,0 +1,14 @@
+package com.example.healthapp.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.healthapp.models.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,String>
+ {
+Optional<User>  findByEmail(String email);
+Optional<User> findByEmailAndPassword(String email, String password); 
+}
